c4c68e2995c7f5f30a531835c99f57b48afa2952
georgevetticaden <george.vetticaden@gmail.com>
2018-06-29 00:08:26 -0700

    cleanup

--- src/main/resources/scripts/smm/cleanupKafkaTopics.sh
+++ src/main/resources/scripts/smm/cleanupKafkaTopics.sh
@@ -15,6 +15,17 @@ else
 	/usr/hdp/current/kafka-broker/bin/kafka-topics.sh --zookeeper $1 --topic syndicate-battery --delete ;
 	/usr/hdp/current/kafka-broker/bin/kafka-topics.sh --zookeeper $1 --topic syndicate-oil --delete ;
 	/usr/hdp/current/kafka-broker/bin/kafka-topics.sh --zookeeper $1 --topic syndicate-transmission --delete ;
+	/usr/hdp/current/kafka-broker/bin/kafka-topics.sh --zookeeper $1 --topic fleet-supply-chain --delete ;
+	/usr/hdp/current/kafka-broker/bin/kafka-topics.sh --zookeeper $1 --topic route-planning --delete ;
+	/usr/hdp/current/kafka-broker/bin/kafka-topics.sh --zookeeper $1 --topic load-optimization --delete ;
+	/usr/hdp/current/kafka-broker/bin/kafka-topics.sh --zookeeper $1 --topic fuel-logistics --delete ;
+	/usr/hdp/current/kafka-broker/bin/kafka-topics.sh --zookeeper $1 --topic supply-chain --delete ;
+	/usr/hdp/current/kafka-broker/bin/kafka-topics.sh --zookeeper $1 --topic predictive-alerts --delete ;
+	/usr/hdp/current/kafka-broker/bin/kafka-topics.sh --zookeeper $1 --topic energy-mgmt --delete ;
+	/usr/hdp/current/kafka-broker/bin/kafka-topics.sh --zookeeper $1 --topic audit-events --delete ;
+	/usr/hdp/current/kafka-broker/bin/kafka-topics.sh --zookeeper $1 --topic compliance --delete ;
+	/usr/hdp/current/kafka-broker/bin/kafka-topics.sh --zookeeper $1 --topic adjudication --delete ;
+	/usr/hdp/current/kafka-broker/bin/kafka-topics.sh --zookeeper $1 --topic approval --delete ;
 	
 	
 fi
