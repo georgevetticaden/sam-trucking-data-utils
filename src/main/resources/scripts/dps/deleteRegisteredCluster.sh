053b0adaf478a82a28d62555e36638389cde4c11
georgevetticaden <george.vetticaden@gmail.com>
2018-07-25 10:03:27 -0500

    adding script for deleting registered

---
+++ src/main/resources/scripts/dps/deleteRegisteredCluster.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ $# -ne 3 ]; then
+        echo "Please pass DataPlane Host to run as the first argument, cluster name as second arg and data center as as third arg"
+else
+        deleteRequestURL="https://$1/api/lakes?clusterName=$2&datacenterName=$3"
+
+        echo "Delete Request URL is: " $deleteRequestURL
+
+        curl -k -b "dp_jwt=$DP_JWT; hadoop-jwt=$HADOOP_JWT;" -X DELETE "$deleteRequestURL"
+fi
