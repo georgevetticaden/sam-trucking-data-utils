28d64613294e49981a3d522cf110d798bbacdc9d
georgevetticaden <george.vetticaden@gmail.com>
2019-06-11 07:15:13 -0500

    reset for srm demo

--- src/main/resources/scripts/smm/createKafkaTopics-srm.sh
+++ src/main/resources/scripts/smm/createKafkaTopics-srm.sh
@@ -2,56 +2,56 @@
 if [ $# -ne 1 ]; then
         echo "Please pass ZK:Port to run as the first argument."
 else
-	/usr/hdp/current/kafka-broker/bin/kafka-topics.sh \
+	/usr/hdf/current/kafka-broker/bin/kafka-topics.sh \
 	--create \
 	--zookeeper $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic gateway-west-raw-sensors;
 
-	/usr/hdp/current/kafka-broker/bin/kafka-topics.sh \
+	/usr/hdf/current/kafka-broker/bin/kafka-topics.sh \
 	--create \
 	--zookeeper $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic gateway-central-raw-sensors;
 
-	/usr/hdp/current/kafka-broker/bin/kafka-topics.sh \
+	/usr/hdf/current/kafka-broker/bin/kafka-topics.sh \
 	--create \
 	--zookeeper $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic gateway-east-raw-sensors;
 	
-	/usr/hdp/current/kafka-broker/bin/kafka-topics.sh \
+	/usr/hdf/current/kafka-broker/bin/kafka-topics.sh \
 	--create \
 	--zookeeper $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic gateway-europe-raw-sensors;	
 
-	/usr/hdp/current/kafka-broker/bin/kafka-topics.sh \
+	/usr/hdf/current/kafka-broker/bin/kafka-topics.sh \
 	--create \
 	--zookeeper $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic syndicate-geo-event-avro;
 
-	/usr/hdp/current/kafka-broker/bin/kafka-topics.sh \
+	/usr/hdf/current/kafka-broker/bin/kafka-topics.sh \
 	--create \
 	--zookeeper $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic syndicate-speed-event-avro;
 
-	/usr/hdp/current/kafka-broker/bin/kafka-topics.sh \
+	/usr/hdf/current/kafka-broker/bin/kafka-topics.sh \
 	--create \
 	--zookeeper $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic syndicate-geo-event-json;
 
-	/usr/hdp/current/kafka-broker/bin/kafka-topics.sh \
+	/usr/hdf/current/kafka-broker/bin/kafka-topics.sh \
 	--create \
 	--zookeeper $1 \
 	--replication-factor 2 \
@@ -59,28 +59,28 @@ else
 	--topic syndicate-speed-event-json;
 
 	
-	/usr/hdp/current/kafka-broker/bin/kafka-topics.sh \
+	/usr/hdf/current/kafka-broker/bin/kafka-topics.sh \
 	--create \
 	--zookeeper $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic syndicate-oil;	
 	
-	/usr/hdp/current/kafka-broker/bin/kafka-topics.sh \
+	/usr/hdf/current/kafka-broker/bin/kafka-topics.sh \
 	--create \
 	--zookeeper $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic syndicate-battery;
 	
-	/usr/hdp/current/kafka-broker/bin/kafka-topics.sh \
+	/usr/hdf/current/kafka-broker/bin/kafka-topics.sh \
 	--create \
 	--zookeeper $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic syndicate-transmission;
 	
-	/usr/hdp/current/kafka-broker/bin/kafka-topics.sh \
+	/usr/hdf/current/kafka-broker/bin/kafka-topics.sh \
 	--create \
 	--zookeeper $1 \
 	--replication-factor 2 \
@@ -88,14 +88,14 @@ else
 	--topic syndicate-all-geo-critical-events;
 	
     
-     /usr/hdp/current/kafka-broker/bin/kafka-topics.sh \
+     /usr/hdf/current/kafka-broker/bin/kafka-topics.sh \
     --create \
     --zookeeper $1 \
     --replication-factor 2 \
     --partitions 3 \
     --topic gateway-geo-raw-sensor;    
     
-     /usr/hdp/current/kafka-broker/bin/kafka-topics.sh \
+     /usr/hdf/current/kafka-broker/bin/kafka-topics.sh \
     --create \
     --zookeeper $1 \
     --replication-factor 2 \
